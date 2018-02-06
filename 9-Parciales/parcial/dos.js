da303ddba259bc14d9949cf3fcfe7ba84c90dac1
DivJaldin <jaldin.ezequiel-@hotmail.com>
2018-02-06 12:35:01 -0300

    clase 2 2 06/02

--- 9-Parciales/parcial/dos.js
+++ 9-Parciales/parcial/dos.js
@@ -1,4 +1,15 @@
 function Mostrar()
 {
-  
+  var importe
+  var iva
+  var importeFinal
+
+    importe = prompt ("importeFinal");
+    importe = parseInt (importe);
+
+    iva = importe * 0.21;
+
+    importeFinal = importe + iva;
+
+document.getElementById ("importeFinal").value = importeFinal;
 }
--- 9-Parciales/parcial/uno.js
+++ 9-Parciales/parcial/uno.js
@@ -1,5 +1,13 @@
 
 function Mostrar()
 {
+ var numerolaBase
+ var perimetro
 
+        numerolaBase = document.getElementById ("laBase").value;
+        numerolaBase = parseInt (numerolaBase);
+
+        perimetro = (numerolaBase * 4);
+
+ alert ("el perimetro es " + perimetro);
 }
