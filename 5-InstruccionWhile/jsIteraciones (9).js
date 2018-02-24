b54ec546620dbdef3c5b1128d7201bc4ec6c15a5
DivJaldin <jaldin.ezequiel-@hotmail.com>
2018-02-24 17:25:33 -0300

    repaso

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,18 +1,50 @@
 function Mostrar()
 {
-
+	var contador = 0;
+	var acumulador = 0;
+	var maximo ;
+	var minimo ;
 	var contador=0;
+	var flag=0;
 	// declarar variables
 	
-	var respuesta='si';
+	var respuesta=true;
 
-	while(respuesta!='no')
+	while(respuesta==true)
 	{
-		
-	
+		numero = prompt ("Ingrese un numero");
+		numero = paseInt (numero);
+
+		while (numero <1 || numero >10 )
+		{
+			numero = prompt ("reingrese un numero");
+			numero = parseInt (numero);
+		}
+
+		if (flag==0)
+		{
+			maximo = numero;
+			minimo = numero;
+			flag=1;
+		}
+
+		else
+		{
+			if (numero>maximo)
+			{
+			maximo = numero;
+			}
+			if (numero<minimo)
+			{
+			minimo= numero;
+			}
+		}
+	acumulador - acuamulador+ numero;
+	contador = contador +1
 	}
-
-
+alert ("Minimo:  " + minimo + "Maximo" + maximo )
+document.getElementById('suma').value=acumulador;
+document.getElementById('promedio').value=acumulador/5;
 
 
 }//FIN DE LA FUNCIÓN
