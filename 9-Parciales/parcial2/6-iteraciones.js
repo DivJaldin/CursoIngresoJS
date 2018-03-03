ac08c16c420ad2cc2a8d91283a6e65648e50aa43
DivJaldin <jaldin.ezequiel-@hotmail.com>
2018-03-03 12:56:05 -0300

    ultimos cambios

--- 9-Parciales/parcial2/6-iteraciones.js
+++ 9-Parciales/parcial2/6-iteraciones.js
@@ -7,13 +7,15 @@ function Mostrar()
 	var i=0;
 	var maximoImporte;
 	var minimoImporte;
-	var flag; 
+	var flag=false; 
+	var maximoDia;
+	var minimoDia;
 	//bucle de dias 7//
 	while (i<7) 
 	{
 		importe = prompt ("ingrese un importe");
 		importe = parseFloat(importe);
-/*no voy a utilizar if ya que solamente se ejecutara una sola vez en este buble
+/*no voy a utilizar if ya que solamente se ejecutara una sola vez en este bucle
 por lo tanto utilizo otro while. genero bucle condicionante dle importe a ingresa
 no exite dia negativo asi que*/
 		while (importe <=0)
@@ -21,7 +23,34 @@ no exite dia negativo asi que*/
 			importe = prompt ("ERROR!!! Reingrese un importe");
 			importe = parseFloat(importe);
 		}
-	// MODO CON BANDERA
+		/////CON BANDERA DENTRO DE IF (ACHICA EL CODIGO)
+	
+			if (flag ==false || importe < minimoImporte)
+			{
+				minimoImporte = importe;
+
+			}
+			if (flag == false || importe > maximoImporte)
+			{
+				maximoImporte = importe;
+				flag=true;
+			} 
+		i++;
+	}
+		alert ("el maximo es: " + maximoImporte + " y el minimo importe es " + minimoImporte)
+}
+
+
+
+
+
+
+
+
+
+
+
+	/*// MODO CON BANDERA
 		//el flag hara que el siguiente if solamente se ejecute una determinada cantidad de veces
 		if (flag == 0)
 		{
@@ -40,13 +69,11 @@ no exite dia negativo asi que*/
 				maximoImporte = importe;
 			} 
 		i++;
-		}
-		alert ("el maximo es: " + maximoImporte + " y el minimo importe es " + minimoImporte)
-	}
-}
+		}*/
 // ESTABLESCO REFERENCIAS/CONDICIONANTES - MODO - SIN BANDERA //
 ///////////////////////////////////////////
-/* MODO 1 
+/* 
+MODO 1 
 		if (i==0)
 		{
 			maximoImporte = importe;
@@ -63,92 +90,6 @@ no exite dia negativo asi que*/
 
 			}
 		}
-//////////////////////////////////////////*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*{
-	var contador=0 ;
-	var detector = false;
-	var bandera;
-	var max;
-	var min;
-
-	alert ("Por favor, ingrese importe de ventas correspondiente a los siguientes días.");
-	do
-	{
-		var lunes = parseInt (prompt("Lunes"));
-		var martes = parseInt (prompt("Martes"));
-		var miercoles = parseInt (prompt("Miercoles"));
-		var jueves = parseInt (prompt ("Jueves"));
-		var viernes  = parseInt (prompt ("viernes "));
-		var sabado = parseInt (prompt ("Sabado"));
-		var domingo = parseInt (prompt ("Domingo"));
-		detector = lunes<0 || martes<0 || miercoles<0 || jueves<0 || viernes <0 || sabado<0 || domingo<0 || isNaN(lunes, martes, miercoles, jueves, viernes , sabado, domingo) ;
-		if(detector == true)
-		{
-			alert ("reingrese los datos por favor.");
-			detector = false;
-		}
-	}
-	while ( lunes<0 || martes<0 || miercoles<0 || jueves<0 || viernes <0 || sabado<0 || domingo<0 || isNaN(lunes, martes, miercoles, jueves, viernes , sabado, domingo))
-	max = lunes;
-	if (martes > max)
-	{
-		max = martes;
-	}
-	if (miercoles >max)
-	{
-		max = miercoles;
-	}
-	if (jueves > max)
-	{
-		max = jueves;
-	}
-	if (viernes > max)
-	{
-		max = viernes;
-	}
-	if (sabado > max)
-	{
-		max = sabado;
-	}
-	if (domingo > max)
-	{
-		max = domingo
-
-
-
-
-
-	}
-	//	bandera = false;
-	//	maximo =num;
-	//	minimo=num;
-	//}	
-	//else
-	//{
-	//	 (numero > maximo)
-
-	//}
-
-}*/
+//////////////////////////////////////////
+*/ 
 
--- 9-Parciales/parcial2/7-iteraciones.js
+++ 9-Parciales/parcial2/7-iteraciones.js
@@ -1,7 +1,62 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 7-iteraciones");
+/* 
+7-Realizar el algoritmo que al presionar el botón "Mostrar" 
+pida por prompt las notas (validar entre 0 y 10) y el sexo(validar el sexo “f” o “m”)
+ de 6 alumnos, informar por alert: 
+a) el promedio de las notas totales. 
+b) la nota más baja. 
+c) la cantidad de varones que su nota haya sido mayor o igual a 6.
+*/
+	var nota;
+	var sexo;
+	/// i es igual contador
+	var i; //no va con =0 por usar el FOR 
+	var acumNotas=0
+	var promedio;
+	var minimo;
+	var contVarones =0;
+	var flag = 0;
+
+	for(i=0; i<6; i++)
+	{
+		nota = prompt ("Por favor ingrese las 6 notas correspondientes");
+		nota = parseInt (nota);
+
+		while (nota<0 || nota>10)
+		{
+			nota = prompt ("ERROR, Reingrese nota valida");
+		}
+        ////* una vez validadas la notas ingresadas proseguimos con el ingreso del sexo y su validacion//////
+		
+		sexo = prompt ("Ingrese sexo");
+
+		while ( sexo!="m" && sexo !="f")
+		{
+			sexo = prompt ("ERROR, Reingrese valores de sexo que sean validas");
+			sexo = sexo.toLowerCase ();
+		}
+		
+		/*while ( isNaN(sexo) == true && sexo!="m" && sexo !="f")
+		*/
+
+		acumNotas = acumNotas + nota;
+
+		if ( flag==0 || nota < minimo )
+		{
+			minimo = nota;
+			flag = 1 ;
+		}
 	
+		if (sexo == "m" && nota >=6)
+		{
+			contVarones++;
+		}
+
+		promedio = acumNotas / i;
+		alert ("Promedio es : "+ promedio + " , el minimo : " + minimo + " y la cantidad de variones " + contVarones)
+	}
+
 }
 
